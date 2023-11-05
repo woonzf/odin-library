256c4493e46eb1c753969f3a8177af48d6e87fd1
woonzf <137765412+woonzf@users.noreply.github.com>
2023-11-05 16:27:25 +0800

    add a dialog for adding a book

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
+// Global variable
+let data = [];
+
 // Run after DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
-    const addBook = document.querySelector("#addBook");
-    const removeBook = document.querySelectorAll("#removeBook");
+    const addBookBtn = document.querySelector("#addBook");
+    const removeBookBtn = document.querySelectorAll("#removeBook");
 
-    // Add a book
-    addBook.addEventListener("click", () => {
-        console.log("add")
+    // Open add a book dialog
+    addBookBtn.addEventListener("click", () => {
+        const addBookDialog = document.querySelector("dialog");
+        const closeDialogBtn = addBookDialog.querySelector("#close");
+        const add = addBookDialog.querySelector("#add");
+
+        addBookDialog.showModal();
+
+        // Close dialog
+        closeDialogBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            addBookDialog.close();
+        })
+
+        // Add the book
+        add.addEventListener("click", (e) => {
+            const title = addBookDialog.querySelector("#title");
+            const author = addBookDialog.querySelector("#author");
+            const pages = addBookDialog.querySelector("#pages");
+            const read = addBookDialog.querySelector("#read");
+            
+            console.log(read.value)
+            
+            data = [title.value, author.value, pages.value];
+
+            if (!data.includes("")) {
+                e.preventDefault();
+                console.log(data)
+                clearData();
+                console.log(data)
+                addBookDialog.close();
+            }
+        })
     })
 
     // Remove book
-    removeBook.forEach(button => {
+    removeBookBtn.forEach(button => {
         button.addEventListener("click", () => {
             console.log("remove")
         })
     })
 })
+
+// Functions
+// Clear input values and data
+const clearData = () => {
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    data = [];
+}
